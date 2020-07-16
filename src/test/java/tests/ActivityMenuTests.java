b5bb12954cadbce44b0a86760a70804e78e7746e
LenaSyrota <lena.syrota@gmail.com>
2020-07-16 08:53:51 +0300

    Adding Sel#18

--- src/test/java/tests/ActivityMenuTests.java
+++ src/test/java/tests/ActivityMenuTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.selenium.pages.*;
+import util.DataProviders;
 //import pages.*;
 
 public class ActivityMenuTests extends TestBase{
@@ -28,57 +29,19 @@ public class ActivityMenuTests extends TestBase{
         qaHaifa56Page.openCurrentBoard();
         qaHaifa56Page.waitUntilPageIsLoaded();
     }
-    @Test
-    public void addingNewListEventInActivity(){
-        String listTitle = "Activity new";
+   @Test(dataProviderClass = DataProviders.class,dataProvider = "DPActivityMenuTests")
+    public void addingNewListEventInActivity(String listTitle){
+        //String listTitle= "Activity new";
         qaHaifa56Page.createNewList(listTitle);
-        upperMenuPage.openMenuPage();
-        upperMenuPage.waitUntilPageIsLoaded();
-        upperMenuPage.openActivityPage();
+        upperMenuPage.openMenuPage()
+                     .waitUntilPageIsLoaded()
+                     .openActivityPage();
         activityPage.waitUntilPageIsLoaded();
 
-        Assert.assertTrue(activityPage.getLastActivityText().contains(" added list "+ listTitle + " to "),
-                "The text in the last activity record doesn't correspond to event adding new list " + listTitle );
 
-
-
+        Assert.assertTrue(activityPage.getLastActivityText().contains("added list"+listTitle+" to "),
+                "The text in the last activity record doesn't correspond to event adding new list "+ listTitle);
 
     }
-//    LoginPageHelper loginPage;
-//    BoardsPageHelper boardsPage;
-//    CurrentBoardHelper qaHaifa56Page;
-//    UpperMenuHelper upperMenuPage;
-//    ActivityPageHelper activityPage;
-//
-//    @BeforeMethod
-//    public void initTests() throws InterruptedException {
-//
-//        loginPage = PageFactory.initElements(driver,LoginPageHelper.class);
-//        boardsPage = PageFactory.initElements(driver, BoardsPageHelper.class);
-//        qaHaifa56Page = new CurrentBoardHelper(driver, BOARD_TITLE);
-//        upperMenuPage= PageFactory.initElements(driver, UpperMenuHelper.class);
-//        activityPage= PageFactory.initElements(driver, ActivityPageHelper.class);
-//
-//        loginPage.openLoginPage();
-//        loginPage.loginAsAtlassian(LOGIN,PASSWORD);
-//        boardsPage.waitUntilPageIsLoaded();
-//        qaHaifa56Page.openCurrentBoard();
-//        qaHaifa56Page.waitUntilPageIsLoaded();
-//
-//    }
-//
-//    @Test
-//    public void addingNewListEventInActivity(){
-//        String listTitle= "Activity new";
-//        qaHaifa56Page.createNewList(listTitle);
-//        upperMenuPage.openMenuPage();
-//        upperMenuPage.waitUntilPageIsLoaded();
-//        upperMenuPage.openActivityPage();
-//        activityPage.waitUntilPageIsLoaded();
-//
-//
-//        Assert.assertTrue(activityPage.getLastActivityText().contains("added list"+listTitle+" to "),
-//                "The text in the last activity record doesn't correspond to event adding new list "+ listTitle);
-//
-//    }
+
 }
